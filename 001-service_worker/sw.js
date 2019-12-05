cea54f4659332e2d0d95560296906ce075bbce78
tony_ojeda <tojeda96@gmai.com>
2019-12-05 15:14:54 -0500

    pwa basic: insert and modify requests

--- 001-service_worker/sw.js
+++ 001-service_worker/sw.js
@@ -1,9 +1,20 @@
 self.addEventListener('fetch', event => {
-    if (event.request.url.includes('.jpg')) {
-        console.log(event.request.url);
-        // let fotoReq = fetch('img/main.jpg');
-        // let fotoReq = fetch(event.request.url);
-        let fotoReq = fetch(event.request);
-        event.respondWith(fotoReq);
+    // if (event.request.url.includes('style.css')) {
+    //     let respuesta = new Response(`
+    //         body {
+    //             background-color: tomato;
+    //             color: pink;
+    //         }
+    //     `, {
+    //         headers: {
+    //             'Content-Type': 'text/css'
+    //         }
+    //     });
+    //     event.respondWith(respuesta);
+    // }
+
+    if (event.request.url.includes('main.jpg')) {
+        let resp = fetch('img/main-patas-arriba.jpg');
+        event.respondWith(resp);
     }
-})
+});
