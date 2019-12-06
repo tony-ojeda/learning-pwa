72e7c5bbc949f5759360f277344602e992a14eea
tony_ojeda <tojeda96@gmai.com>
2019-12-06 18:01:33 -0500

    cache service worker: operations with cache

--- 003-cache_offline/sw.js
+++ 003-cache_offline/sw.js
@@ -1,28 +1,34 @@
 self.addEventListener('fetch', event => {
 
-    const offlineResponse = new Response(`
+    // const offlineResponse = new Response(`
 
-        <!DOCTYPE html>
-    <html lang="en">
+    //     Bienvenido a mi Página Web
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Mi PWA</title>
-    </head>
+    //     Disculpa,pero para usarla, necesitas internet
 
-    <body class="container p-3">
-        <h1> Offline Mode</h1>
-    </body>
-    </html>
-        `, {
-        headers: {
-            'Content-Type': 'text/html'
-        }
-    });
+    // `);
 
+    //     const offlineResponse = new Response(`
 
+    //     <!DOCTYPE html>
+    // <html lang="en">
+
+    // <head>
+    //     <meta charset="UTF-8">
+    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    //     <title>Mi PWA</title>
+    // </head>
+
+    // <body class="container p-3">
+    //     <h1> Offline Mode</h1>
+    // </body>
+    // </html>
+    //     `, {
+    //         headers: {
+    //             'Content-Type': 'text/html'
+    //         }
+    //     });
 
     const offlineResp = fetch('pages/offline.html')
 
