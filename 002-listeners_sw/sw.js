96a9d67d33fb0948f0a26525e8cd23840fcb121f
tony_ojeda <tojeda96@gmai.com>
2019-12-06 05:38:51 -0500

    listeners services worker: skip waiting

--- 002-listeners_sw/sw.js
+++ 002-listeners_sw/sw.js
@@ -3,10 +3,11 @@ self.addEventListener('install', event => {
     // descargar assets
     // creamos un cache
     console.log('instalando SW');
+    self.skipWaiting();
 });
 
 // cuando el SW toma el control de la apliación
 self.addEventListener('activate', event => {
     // borrar cache viejo
-    console.log('SW: activo y listo para controlar la app');
+    console.log('SW2: activo y listo para controlar la app');
 });
