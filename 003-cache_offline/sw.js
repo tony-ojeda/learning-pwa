ee8a858de8ea6c916d40344b8538135a60009e07
tony_ojeda <tojeda96@gmai.com>
2019-12-06 16:34:42 -0500

    cache service worker: response html string

--- 003-cache_offline/sw.js
+++ 003-cache_offline/sw.js
@@ -2,11 +2,25 @@ self.addEventListener('fetch', event => {
 
     const offlineResponse = new Response(`
 
-        Bienvenido a mi Página Web
+        <!DOCTYPE html>
+    <html lang="en">
 
-        Disculpa,pero para usarla, necesitas internet
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Mi PWA</title>
+    </head>
 
-    `);
+    <body class="container p-3">
+        <h1> Offline Mode</h1>
+    </body>
+    </html>
+        `, {
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    });
 
 
 
