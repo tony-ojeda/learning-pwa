42617fef50f9846d4e7645a4e89791443362ddd7
tony_ojeda <tojeda96@gmai.com>
2019-12-06 07:23:53 -0500

    listeners services worker: fetch

--- 002-listeners_sw/js/app.js
+++ 002-listeners_sw/js/app.js
@@ -2,3 +2,7 @@
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('sw.js');
 }
+
+fetch('https://reqres.in/api/users')
+    .then(resp => resp.text())
+    .then(console.log);
--- 002-listeners_sw/sw.js
+++ 002-listeners_sw/sw.js
@@ -3,7 +3,19 @@ self.addEventListener('install', event => {
     // descargar assets
     // creamos un cache
     console.log('instalando SW');
-    self.skipWaiting();
+
+    const instalación = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('SW: Instalaciones terminadas');
+            self.skipWaiting();
+            resolve();
+        }, 5);
+
+    })
+
+
+    event.waitUntil(instalación);
+
 });
 
 // cuando el SW toma el control de la apliación
@@ -11,3 +23,15 @@ self.addEventListener('activate', event => {
     // borrar cache viejo
     console.log('SW2: activo y listo para controlar la app');
 });
+
+// FETCH: manejo de peticiones HTTP
+self.addEventListener('fetch', event => {
+    // apicar estrategias de cache
+    console.log('SW:', event.request.url);
+
+    if (event.request.url.includes('https://reqres.in/')) {
+        const resp = new Response(`{ok: false, mensaje: 'jajaja :v'}`);
+
+        event.respondWith(resp);
+    }
+})
