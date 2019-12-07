cc1da0e78d5142b49a06da8c2c39445c66ee22b1
tony_ojeda <tojeda96@gmai.com>
2019-12-06 20:13:43 -0500

    cache service worker: app shell save files keys of my app

---
+++ 003-cache_offline/sw-basic.js
@@ -0,0 +1,38 @@
+self.addEventListener('fetch', event => {
+
+    // const offlineResponse = new Response(`
+
+    //     Bienvenido a mi Página Web
+
+    //     Disculpa,pero para usarla, necesitas internet
+
+    // `);
+
+    //     const offlineResponse = new Response(`
+
+    //     <!DOCTYPE html>
+    // <html lang="en">
+
+    // <head>
+    //     <meta charset="UTF-8">
+    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    //     <title>Mi PWA</title>
+    // </head>
+
+    // <body class="container p-3">
+    //     <h1> Offline Mode</h1>
+    // </body>
+    // </html>
+    //     `, {
+    //         headers: {
+    //             'Content-Type': 'text/html'
+    //         }
+    //     });
+
+    const offlineResp = fetch('pages/offline.html')
+
+    const resp = fetch(event.request)
+        .catch(() => offlineResponse);
+    event.respondWith(resp);
+});
--- 003-cache_offline/sw.js
+++ 003-cache_offline/sw.js
@@ -1,38 +1,14 @@
-self.addEventListener('fetch', event => {
+self.addEventListener('install', e => {
+    const cacheProm = caches.open('cache-1')
+        .then(cache => {
+            return cache.addAll([
+                'index.html',
+                'css/style.css',
+                'img/main.jpg',
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+                'js/app.js'
+            ]);
+        });
 
-    // const offlineResponse = new Response(`
-
-    //     Bienvenido a mi Página Web
-
-    //     Disculpa,pero para usarla, necesitas internet
-
-    // `);
-
-    //     const offlineResponse = new Response(`
-
-    //     <!DOCTYPE html>
-    // <html lang="en">
-
-    // <head>
-    //     <meta charset="UTF-8">
-    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    //     <title>Mi PWA</title>
-    // </head>
-
-    // <body class="container p-3">
-    //     <h1> Offline Mode</h1>
-    // </body>
-    // </html>
-    //     `, {
-    //         headers: {
-    //             'Content-Type': 'text/html'
-    //         }
-    //     });
-
-    const offlineResp = fetch('pages/offline.html')
-
-    const resp = fetch(event.request)
-        .catch(() => offlineResponse);
-    event.respondWith(resp);
-});
+    e.waitUntil(cacheProm);
+})
