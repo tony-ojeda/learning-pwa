4ef74eb31897b97598acc6f762880b98609892c7
tony_ojeda <tojeda96@gmai.com>
2019-12-07 05:35:37 -0500

    cache service worker: cache strategies - cache dinamic with limit

--- 003-cache_offline/sw.js
+++ 003-cache_offline/sw.js
@@ -1,19 +1,37 @@
-const CACHE_NAME = 'cache-1';
+// const CACHE_NAME = 'cache-1';
+const CACHE_STATIC_NAME = 'static-v2';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+function limpiarCache(cacheName, numeroItems) {
+    caches.open(cacheName)
+        .then(cache => {
+            return cache.keys()
+                .then(keys => {
+                    if (keys.length > numeroItems) {
+                        cache.delete(keys[0])
+                            .then(limpiarCache(cacheName, numeroItems));
+                    }
+                });
+        });
+}
 
 self.addEventListener('install', e => {
-    const cacheProm = caches.open(CACHE_NAME)
+    const cacheProm = caches.open(CACHE_STATIC_NAME)
         .then(cache => {
             return cache.addAll([
                 '/',
                 'index.html',
                 'css/style.css',
                 'img/main.jpg',
-                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
                 'js/app.js'
             ]);
         });
 
-    e.waitUntil(cacheProm);
+    const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
+        .then(cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
+
+    e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 });
 
 self.addEventListener('fetch', e => {
@@ -26,9 +44,10 @@ self.addEventListener('fetch', e => {
             console.log('No existe', e.request.url);
 
             return fetch(e.request).then(newResp => {
-                caches.open(CACHE_NAME)
+                caches.open(CACHE_DYNAMIC_NAME)
                     .then(cache => {
                         cache.put(e.request, newResp);
+                        limpiarCache(CACHE_DYNAMIC_NAME, 50);
                     });
                 return newResp.clone();
             });
