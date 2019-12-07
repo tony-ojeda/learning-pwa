9b95cb523f4a85217c4718e00c1d67abba0c39fe
tony_ojeda <tojeda96@gmai.com>
2019-12-07 06:06:54 -0500

    cache service worker: cache strategies - network with cache fallback

--- 003-cache_offline/sw.js
+++ 003-cache_offline/sw.js
@@ -2,6 +2,7 @@
 const CACHE_STATIC_NAME = 'static-v2';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+const CACHE_DYNAMIC_LIMIT = 50;
 
 function limpiarCache(cacheName, numeroItems) {
     caches.open(cacheName)
@@ -35,25 +36,44 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('fetch', e => {
-    // 2- Cache with Network Fallback
-    const respuesta = caches.match(e.request)
-        .then(res => {
-            if (res) return res;
-            // No existe el archivo
-            // tengo que ir a la web
-            console.log('No existe', e.request.url);
 
-            return fetch(e.request).then(newResp => {
-                caches.open(CACHE_DYNAMIC_NAME)
-                    .then(cache => {
-                        cache.put(e.request, newResp);
-                        limpiarCache(CACHE_DYNAMIC_NAME, 50);
-                    });
-                return newResp.clone();
-            });
-        });
+    // 3- Network with cache fallback
+    const respuesta = fetch(e.request).then(res => {
+        if (!res) return caches.match(e.request);
+        console.log('Fetch:', res);
+        caches.open(CACHE_DYNAMIC_NAME)
+            .then(cache => {
+                cache.put(e.request, res);
+                limpiarCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT);
+            })
+
+        return res.clone();
+    }).catch(err => {
+        return caches.match(e.request)
+    })
 
     e.respondWith(respuesta);
+
+
+    // 2- Cache with Network Fallback
+    // const respuesta = caches.match(e.request)
+    //     .then(res => {
+    //         if (res) return res;
+    //         // No existe el archivo
+    //         // tengo que ir a la web
+    //         console.log('No existe', e.request.url);
+
+    //         return fetch(e.request).then(newResp => {
+    //             caches.open(CACHE_DYNAMIC_NAME)
+    //                 .then(cache => {
+    //                     cache.put(e.request, newResp);
+    //                     limpiarCache(CACHE_DYNAMIC_NAME, 50);
+    //                 });
+    //             return newResp.clone();
+    //         });
+    //     });
+
+    // e.respondWith(respuesta);
 
     // 1- Cache Only
     // e.respondWith(caches.match(e.request))
