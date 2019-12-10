877bee2d454d33b37feebd9d47a14f82a03ef0b5
tony_ojeda <tojeda96@gmai.com>
2019-12-09 21:10:33 -0500

    cache service worker: cache strategies - cache & Nertwork Race

--- 003-cache_offline/sw.js
+++ 003-cache_offline/sw.js
@@ -25,7 +25,8 @@ self.addEventListener('install', e => {
                 'index.html',
                 'css/style.css',
                 'img/main.jpg',
-                'js/app.js'
+                'js/app.js',
+                'img/no-img.jpg'
             ]);
         });
 
@@ -37,22 +38,50 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
 
-    // 4- Cache with nerwork update
-    // Rendimiento es crítico
-    // Siempre estaran un paso atras 
-    if (e.request.url.includes('bootstrap')) {
-        return e.respondWith(caches.match(e.request));
-    }
+    // 5- Cache & Network Race
+    const respuesta = new Promise((resolve, reject) => {
+        let rechazada = false;
 
-    const respuesta = caches.open(CACHE_STATIC_NAME).then(cache => {
-        fetch(e.request).then(newRes => {
-            cache.put(e.request, newRes);
-            return cache.match(e.request);
-        });
+        const falloUnaVez = () => {
+            if (rechazada) {
+                if (/\.(png|jpg)$/i.test(e.request.url)) {
+                    resolve(caches.match('img/no-img.jpg'));
+                } else {
+                    reject('No se encontro respuesta');
+                }
+            } else {
+                rechazada = true;
+            }
+        };
+
+        fetch(e.request).then(res => {
+            res.ok ? resolve(res) : falloUnaVez();
+        }).catch(falloUnaVez)
+
+        caches.match(e.request).then(res => {
+            res ? resolve(res) : falloUnaVez();
+        }).catch(falloUnaVez)
     });
 
 
     e.respondWith(respuesta);
+
+    // 4- Cache with nerwork update
+    // Rendimiento es crítico
+    // Siempre estaran un paso atras 
+    // if (e.request.url.includes('bootstrap')) {
+    //     return e.respondWith(caches.match(e.request));
+    // }
+
+    // const respuesta = caches.open(CACHE_STATIC_NAME).then(cache => {
+    //     fetch(e.request).then(newRes => {
+    //         cache.put(e.request, newRes);
+    //         return cache.match(e.request);
+    //     });
+    // });
+
+
+    // e.respondWith(respuesta);
 
 
     // 3- Network with cache fallback
