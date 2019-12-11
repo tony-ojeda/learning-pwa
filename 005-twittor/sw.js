74480eb841dfbffd060a48531afe08f080bf19de
tony_ojeda <tojeda96@gmai.com>
2019-12-11 05:44:12 -0500

    twittor project: delete cache old

--- 005-twittor/sw.js
+++ 005-twittor/sw.js
@@ -4,29 +4,39 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
     '/',
-    'index.html',
-    'css/style.css',
-    'img/favicon.ico',
-    'img/avatars/hulk.jpg',
-    'img/avatars/ironman.jpg',
-    'img/avatars/spiderman.jpg',
-    'img/avatars/thor.jpg',
-    'img/avatars/wolverine.jpg',
-    'app.js'
+    '/index.html',
+    '/css/style.css',
+    '/img/favicon.ico',
+    '/img/avatars/hulk.jpg',
+    '/img/avatars/ironman.jpg',
+    '/img/avatars/spiderman.jpg',
+    '/img/avatars/thor.jpg',
+    '/img/avatars/wolverine.jpg',
+    '/js/app.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    'css/animate.css',
-    'js/libs/jquery.js'
+    '/css/animate.css',
+    '/js/libs/jquery.js'
 ];
 
 self.addEventListener('install', e => {
 
     const cacheStatic = caches.open(STATIC_CACHE).then(cache => cache.addAll(APP_SHELL));
-    const cacheInmytable = caches.open(INMUTABLE_CACHE).then(cache => cache.addAll(APP_SHELL_INMUTABLE));
+    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => cache.addAll(APP_SHELL_INMUTABLE));
 
-    e.waintUntil();
-})
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+});
+
+self.addEventListener('activate', e => {
+    const respuesta = caches.keys().then(keys => {
+        keys.forEach(key => {
+            // let staticCacheNameIsDifferent = key !== STATIC_CACHE && key.includes('static');
+            if (key !== STATIC_CACHE && key.includes('static')) return caches.delete(key);
+        });
+    });
+    e.waitUntil(respuesta);
+});
