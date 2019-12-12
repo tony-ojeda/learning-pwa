753244ba6007efde40525a0dc15e8ae225b91bc8
tony_ojeda <tojeda96@gmai.com>
2019-12-12 05:51:37 -0500

    indexedDB: create database and register store (tabla)

--- 006-indexeddb/app.js
+++ 006-indexeddb/app.js
@@ -1,6 +1,13 @@
-
 // indexedDB: Reforzamiento
+let request = window.indexedDB.open('mi-database', 1);
 
+// Se actualiza cuando se crea o se sube de version de la DB
+request.onupgradeneeded = event => {
+    console.log('Actualización de BD');
 
+    let db = event.target.result;
 
-
+    db.createObjectStore('heroes', {
+        keyPath: 'id'
+    });
+};
