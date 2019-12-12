7744784af970c8d204a50d612953cde04e6867d5
tony_ojeda <tojeda96@gmai.com>
2019-12-12 06:14:44 -0500

    indexedDB: adding data in store (tabla)

--- 006-indexeddb/app.js
+++ 006-indexeddb/app.js
@@ -11,3 +11,38 @@ request.onupgradeneeded = event => {
         keyPath: 'id'
     });
 };
+
+// Manejo de errores
+request.onerror = event => {
+    console.log('DB error', event.target.error);
+};
+
+// insertar datos
+request.onsuccess = event => {
+    let db = event.target.result;
+
+    let heroesData = [
+        { id: '1111', heroe: 'Spiderman', mensaje: 'Aquí su amigo Spiderman' },
+        { id: '2222', heroe: 'Ironman', mensaje: 'Aquí en mi nuevo Mark 50' }
+    ];
+
+    let heroesTransaction = db.transaction('heroes', 'readwrite');
+    heroesTransaction.onerror = event => {
+        console.log('Error guardado', event.target.error);
+    }
+
+    // Informa sobre el exito de la transacción
+    heroesTransaction.oncomplete = event => {
+        console.log('Transacción hecha', event);
+    };
+
+    let heroesStore = heroesTransaction.objectStore('heroes');
+
+    for (let heroe of heroesData) {
+        heroesStore.add(heroe);
+    }
+
+    heroesStore.onsuccess = event => {
+        console.log('Nuevo item agregado a la base de datos');
+    }
+}
